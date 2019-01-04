1f40c0fbf2e2ce2571483f97f53f983ca1999665
deartanapol10@gmail.com <deartanapol10@gmail.com>
2019-01-05 05:05:53 +0700

    Updates

--- precache-manifest.76a8397308ad27e8a435b0a3ef12d101.js
+++ precache-manifest.76a8397308ad27e8a435b0a3ef12d101.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "3793f463a455c4061df1",
-    "url": "/portfolio/static/js/main.3793f463.chunk.js"
+    "revision": "a4004e742d500749726e",
+    "url": "/portfolio/static/js/main.a4004e74.chunk.js"
   },
   {
     "revision": "e3321cb90233b3dbeebc",
     "url": "/portfolio/static/js/1.e3321cb9.chunk.js"
   },
   {
-    "revision": "3793f463a455c4061df1",
+    "revision": "a4004e742d500749726e",
     "url": "/portfolio/static/css/main.9f3216d5.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/1.ed507c0f.chunk.css"
   },
   {
-    "revision": "d7b485ad1338fde9c7a40a3335a27029",
+    "revision": "4b28dd78a9d0e350286b886fd674c6f7",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.62ef9601e4d97a0fcbb50694dafaf127.js"
+  "/portfolio/precache-manifest.76a8397308ad27e8a435b0a3ef12d101.js"
 );
 
 workbox.clientsClaim();
